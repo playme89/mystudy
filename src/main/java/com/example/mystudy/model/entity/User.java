5d8667604f5a1d48efbf6280c1742c2a9941025c
jaesung.ko <playme89@naver.com>
2023-02-21 17:18:30 +0900

    second commit

--- src/main/java/com/example/mystudy/model/entity/User.java
+++ src/main/java/com/example/mystudy/model/entity/User.java
@@ -4,15 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
--- src/test/java/com/example/mystudy/repository/UserRepositoryTest.java
+++ src/test/java/com/example/mystudy/repository/UserRepositoryTest.java
@@ -1,53 +1,82 @@
 package com.example.mystudy.repository;
 
+
 import com.example.mystudy.MystudyApplication;
 import com.example.mystudy.model.entity.User;
-import org.assertj.core.api.Assertions;
+
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.test.context.ContextConfiguration;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = MystudyApplication.class)
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
+    @DisplayName("Create 테스트")
     @Test
     public void create() {
         User user = User.builder()
-                .account("test03")
-                .email("playme3@naver.com")
+                .account("test04")
+                .email("playme4@naver.com")
                 .phoneNumber("010-1111-1111")
                 .createdAt(LocalDateTime.now())
-                .createdBy("admin3")
+                .createdBy("admin4")
                 .build();
         User newUser = userRepository.save(user);
-        System.out.println("newUser:"+ newUser);
+        System.out.println("newUser:" + newUser);
     }
 
+    @DisplayName("Read 테스트")
     @Test
-    public void read(@RequestParam Long id) {
-       Optional<User> user = userRepository.findById(2L);
-        user.ifPresent(selectUser->{
+    public void read() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
             System.out.println("user:" + selectUser);
-            System.out.println("email:"+ selectUser.getEmail());
+            System.out.println("email:" + selectUser.getEmail());
+        });
+    }
+    @DisplayName("Update 테스트")
+    @Test
+    public void update() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent( selectUser->{
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method");
+
+            userRepository.save(selectUser);
         });
 
-    }
 
-    public void update() {
 
     }
-
+    @Test
+    @Transactional
     public void delete() {
+        Optional<User> user = userRepository.findById(3L);
 
+        Assertions.assertTrue(user.isPresent());
+        user.ifPresent(selectUser->{ userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        Assertions.assertFalse(deleteUser.isPresent());
     }
 }
